ec3ceb415cce35feb6e1b63fa2134f7a548d926c
nickyadvokaat <nicky.advokaat@nubisonline.nl>
2014-02-19 15:53:38 +0100

    Basic bridge building controls

--- code/BridgeBuilder/src/mygame/Main.java
+++ code/BridgeBuilder/src/mygame/Main.java
@@ -12,6 +12,7 @@ import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Ray;
+import com.jme3.math.Transform;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
@@ -29,7 +30,11 @@ import com.jme3.terrain.geomipmap.TerrainLodControl;
  */
 public class Main extends SimpleApplication {
 
+    private Node clickable;
     private Node anchorPoints;
+    private Node targetPoints;
+    private Node buildingBlocks;
+    private Geometry prevClickedGeometry;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -61,58 +66,80 @@ public class Main extends SimpleApplication {
             lodControl.setCamera(getCamera());
         }
 
-        anchorPoints = new Node("Anchorpoint");
-        rootNode.attachChild(anchorPoints);
+        clickable = new Node("Clickable");
+        rootNode.attachChild(clickable);
 
+        anchorPoints = new Node("AnchorPoints");
+        clickable.attachChild(anchorPoints);
+        targetPoints = new Node("TargetPoints");
+        clickable.attachChild(targetPoints);
+        buildingBlocks = new Node("BuildingBlocks");
+        clickable.attachChild(buildingBlocks);
 
+        addAnchorPoint(new Vector3f(0, 0, -15));
+        addAnchorPoint(new Vector3f(5, 0, -15));
+        addAnchorPoint(new Vector3f(0, 0, 25));
+        addAnchorPoint(new Vector3f(5, 0, 25));
+        addAnchorPoint(new Vector3f(0, -15, 0));
+        addAnchorPoint(new Vector3f(5, -15, 0));
+        addAnchorPoint(new Vector3f(0, -15, 10));
+        addAnchorPoint(new Vector3f(5, -15, 10));
+    }
+
+    private void addAnchorPoint(Vector3f a) {
         Material mat = new Material(assetManager,
                 "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Gray);
 
         Box box = new Box(0.5f, 0.5f, 0.5f);
-        
-        Geometry gm = new Geometry("Box", box);
-        gm.setLocalTranslation(new Vector3f(0, 0, -15));
-        gm.setMaterial(mat);
-
-        Geometry gm2 = new Geometry("Box", box);
-        gm2.setLocalTranslation(new Vector3f(5, 0, -15));
-        gm2.setMaterial(mat);
-
-        Geometry gm3 = new Geometry("Box", box);
-        gm3.setLocalTranslation(new Vector3f(0, 0, 25));
-        gm3.setMaterial(mat);
-
-        Geometry gm4 = new Geometry("Box", box);
-        gm4.setLocalTranslation(new Vector3f(5, 0, 25));
-        gm4.setMaterial(mat);
-        
-        Geometry gm5 = new Geometry("Box", box);
-        gm5.setLocalTranslation(new Vector3f(0, -15, 0));
-        gm5.setMaterial(mat);
-
-        Geometry gm6 = new Geometry("Box", box);
-        gm6.setLocalTranslation(new Vector3f(5, -15, 0));
-        gm6.setMaterial(mat);
-
-        Geometry gm7 = new Geometry("Box", box);
-        gm7.setLocalTranslation(new Vector3f(0, -15, 10));
-        gm7.setMaterial(mat);
 
         Geometry gm8 = new Geometry("Box", box);
-        gm8.setLocalTranslation(new Vector3f(5, -15, 10));
+        gm8.setLocalTranslation(a);
         gm8.setMaterial(mat);
 
-        
-        anchorPoints.attachChild(gm);
-        anchorPoints.attachChild(gm2);
-        anchorPoints.attachChild(gm3);
-        anchorPoints.attachChild(gm4);
-        anchorPoints.attachChild(gm5);
-        anchorPoints.attachChild(gm6);
-        anchorPoints.attachChild(gm7);
+        gm8.setUserData("type", 0);
         anchorPoints.attachChild(gm8);
+    }
 
+    private void addTargetPoint(Vector3f a) {
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Yellow);
+
+        Box box = new Box(0.51f, 0.51f, 0.51f);
+
+        Geometry gm8 = new Geometry("Box", box);
+        gm8.setLocalTranslation(a);
+        gm8.setMaterial(mat);
+
+        gm8.setUserData("type", 1);
+        targetPoints.attachChild(gm8);
+    }
+
+    private void addBuildingBlock(Vector3f from, Vector3f to) {
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.DarkGray);
+
+        Box box = new Box(0.5f, 0.5f, 0.5f);
+        
+        if(from.x == to.x && ! (from.z == to.z)){
+            box = new Box(0.25f, 0.25f, 2.5f);
+        }else if(from.y == to.y ){
+            box = new Box(2.5f, 0.25f, 0.25f);
+        }else if(from.z == to.z){
+            box = new Box(0.25f, 2.5f, 0.25f);
+        }
+        
+        Geometry gm8 = new Geometry("Box", box);
+        gm8.setLocalTranslation(new Vector3f((from.x + to.x) / 2, (from.y + to.y) / 2, (from.z + to.z) / 2));
+        gm8.setMaterial(mat);
+
+        gm8.setUserData("type", 2);
+        buildingBlocks.attachChild(gm8);
+
+        System.out.println(from.toString());
+        System.out.println(to.toString());
     }
 
     @Override
@@ -145,22 +172,56 @@ public class Main extends SimpleApplication {
                 // 2. Aim the ray from cam loc to cam direction.
                 Ray ray = new Ray(cam.getLocation(), cam.getDirection());
                 // 3. Collect intersections between Ray and Anchorpoints in results list.
-                anchorPoints.collideWith(ray, results);
+                clickable.collideWith(ray, results);
                 // 4. Use the results 
                 // reset color
                 Material mat = new Material(assetManager,
                         "Common/MatDefs/Misc/Unshaded.j3md");
                 mat.setColor("Color", ColorRGBA.Gray);
                 anchorPoints.setMaterial(mat);
+
                 // apply new
                 if (results.size() > 0) {
                     // The closest collision point is what was truly hit:
                     CollisionResult closest = results.getClosestCollision();
+                    Geometry geometry = closest.getGeometry();
 
-                    Material mat2 = new Material(assetManager,
-                            "Common/MatDefs/Misc/Unshaded.j3md");
-                    mat2.setColor("Color", ColorRGBA.Red);
-                    closest.getGeometry().setMaterial(mat2);
+                    switch ((Integer) (geometry.getUserData("type"))) {
+                        case 0: // anchor
+                            // reset target
+                            targetPoints.detachAllChildren();
+
+                            Material mat2 = new Material(assetManager,
+                                    "Common/MatDefs/Misc/Unshaded.j3md");
+                            mat2.setColor("Color", ColorRGBA.Red);
+                            geometry.setMaterial(mat2);
+
+                            prevClickedGeometry = geometry;
+
+                            Vector3f translation = closest.getGeometry().getLocalTransform().getTranslation();
+
+                            addTargetPoint(new Vector3f(translation.x, translation.y + 5, translation.z));
+                            addTargetPoint(new Vector3f(translation.x, translation.y - 5, translation.z));
+                            addTargetPoint(new Vector3f(translation.x + 5, translation.y, translation.z));
+                            addTargetPoint(new Vector3f(translation.x, translation.y, translation.z + 5));
+                            addTargetPoint(new Vector3f(translation.x - 5, translation.y, translation.z));
+                            addTargetPoint(new Vector3f(translation.x, translation.y, translation.z - 5));
+                            break;
+                        case 1: // target
+                            addAnchorPoint(geometry.getLocalTranslation());
+                            addBuildingBlock(prevClickedGeometry.getLocalTransform().getTranslation(),
+                                    geometry.getLocalTransform().getTranslation());
+                            // reset target
+                            targetPoints.detachAllChildren();
+                            break;
+                        case 2: // building block
+                            break;
+
+                    }
+                } else {
+
+                    // reset target
+                    targetPoints.detachAllChildren();
                 }
             }
         }
